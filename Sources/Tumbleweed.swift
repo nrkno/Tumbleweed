023ab8c345989306bbc5af56277cce93a6a94c23
Johan Sørensen <johan.sorensen@nrk.no>
2017-04-06 09:52:31 +0200

    initial commit

---
+++ Sources/Tumbleweed.swift
@@ -0,0 +1,9 @@
+//
+//  Tumbleweed.swift
+//  NRK
+//
+//  Created by Johan Sørensen on {TODAY}.
+//  Copyright © 2017 NRK. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import TumbleweedTests
+
+XCTMain([
+    testCase(TumbleweedTests.allTests),
+])
